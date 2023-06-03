ac946094cd7e54e808a5627fde76980097c49b12
diabeets <daiabeets@gmail.com>
2023-06-03 17:12:52 -0400

    second commit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,10 @@
 import './home.css'
 import MyPicture from '../../assets/Untitled.png'
+import Resume from '../../assets/JukaiDaiResume.pdf'
+import GithubIcon from '../../assets/github-mark.svg'
+import LinkedinIcon from '../../assets/linkedin.png'
+import EmailIcon from '../../assets/email.png'
+
 
 function Home(){
     return (
@@ -10,18 +15,30 @@ function Home(){
                 </div>
 
                 <div className='aboutmMe'>
-                    <h1 className='Hi'>Hi </h1>
+                    <h1 className='hi'>Hi! </h1>
                     <p>
                         I'm a fourth year Computer Engineering student
                         graduating this year at Toronto Metropolitan University. 
                         Currently I'm looking for a full time positions.
-                        
-                        
+                                       
                     </p>
 
                 </div>
                 <div className='links'>
-                    <a href="https://github.com/diabeets"> github</a>
+                    <a href="https://github.com/diabeets" 
+                    target='react\jsx-no-target-blank'> 
+                    <img src={GithubIcon} alt ="github" className='icon' />
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/jukai-dai-17548518b/" 
+                    target='react\jsx-no-target-blank' > 
+                    <img src={LinkedinIcon} alt ="github" className='icon' />
+                    </a>
+
+                    <a href="mailto:juk41@gmail.com"
+                    target='react\jsx-no-target-blank' > 
+                    <img src={EmailIcon} alt="juk41@gmail.com" className='icon' />
+                    </a>
                 </div>
             </div>
         </>
