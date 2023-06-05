b70fda8209b7d4e893137fa3010550acb633d39c
diabeets <daiabeets@gmail.com>
2023-06-05 18:45:51 -0400

    homepage update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,23 +13,7 @@ function Home(){
                 <div className='myPic'>
                     <img src={MyPicture} alt="developer" />
                 </div>
-
-                <div className='aboutmMe'>
-                    <h1 className='hi'>Hi! </h1>
-                    <p>
-                        I'm a fourth year Computer Engineering student
-                        graduating this year at Toronto Metropolitan University. 
-                        Currently I'm looking for a full time positions.
-                                       
-                    </p>
-
-                </div>
-
-                <div className='resume'>
-
-                    <ResumeButton />
-
-                </div>
+                <h1>Jukai Dai</h1>
 
                 <div className='links'>
                     <a href="https://github.com/diabeets" 
@@ -47,7 +31,32 @@ function Home(){
                     <img src={EmailIcon} alt="juk41@gmail.com" className='icon' />
                     </a>
                 </div>
+
+                <div className='resume'>
+                    <ResumeButton />
+                </div>                         
+
+                <div className='aboutMe'>
+                    <h2 className='hi'>  About Me</h2>
+                    <p>
+                    Hello, I'm a fourth-year Computer Engineering student graduating this year from Toronto Metropolitan University. 
+                    Currently, I'm looking for a full-time position. Some interests and courses I have taken are computer architecture, 
+                    embedded systems, machine learning, and app and web development.
+                        
+                    </p>
+
+                    <p>
+                    Outside of school, I enjoy playing video games especially competitive team-based games and puzzle games,
+                     I find playing video games allows me to apply my creative side by solving problems as a team and exploring different play styles.
+                    </p>
+
+                    <p>I also create personal projects in my own time to futher learn and expand my 
+                        skillset, you can view my projects in my github page. 
+                    </p>
+
+                </div>
             </div>
+
         </>
     )
 }
